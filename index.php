145fdf0221c9942c930cd1b4ba855ea8c4e961ec
teknow <thiago@teknow.com.br>
2018-08-11 20:21:21 -0300

    agora sim, tinha esquecido de salvar

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 echo "<strong>Olá mundo! 123</strong>";
 
-echo "so pra avacalhar";
-=======
-echo "<strong>Olá mundo!</strong>";
-
-echo "Olá Thiago!" 
->>>>>>> 89c93134750de9761de4e72aa80da5df25ddd177
+echo "so pra avacalhar, Thiago";
 
 ?>
